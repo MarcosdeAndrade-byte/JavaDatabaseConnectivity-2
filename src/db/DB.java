f21ad906726fd21ea7a3efe2dd1e58fa49750a43
Marcos_Alexandre <junior.marcos.mjr@hotmail.com>
2021-11-15 18:24:49 -0300

    Connection

--- src/db/DB.java
+++ src/db/DB.java
@@ -3,13 +3,35 @@ package db;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 
 public class DB {
 	
 private static Connection conn = null;
+	
+    //Método para conectar ao banco de dados
+	public static Connection getConnection() {
+		//Se variável conn for igual a nulo
+		if(conn == null) {
+			try {
+				//Carregamos as conexões para variável props
+				Properties props = loadProperties();
+				//A variável URL do tipo String recebe a propriedade dburl que está no arquivo db.properties
+				String url = props.getProperty("dburl");
+				//conn recebe um método que faz a leitura das propriedades URL e props
+				conn = DriverManager.getConnection(url,props);
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
 
+	
 	//Método para guardar propriedades(Informações do banco de dados que estão na pasta db.properties)
 	private static Properties loadProperties() {
 		//Acessamos o arquivo db.properties
