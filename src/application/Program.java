78c0fe8fb984e427f06c59224e160d0de055301b
Marcos_Alexandre <junior.marcos.mjr@hotmail.com>
2021-11-16 11:17:10 -0300

    RecuperacaoDeDados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,8 +12,36 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		//O Statement serve para executarmos um comando sql que vai recuperar as informações no banco de dados
+		Statement st = null;
+		//Já o ResultSet serve para armazenar os dados(Em forma de tabela)
+		ResultSet rs = null;
+		
+		try {
+			//conn recebe uma conexão com o banco de dados(Nos conectamos com o banco)
+			conn = DB.getConnection();
+			
+			//A variável ST do tipo Statement recebe  conn.createStatement()
+			st = conn.createStatement();
+			
+			//através do método executeQuery podemos passar um Script SQL
+			rs = st.executeQuery("select * from department");
+			
+			//Enquanto houver linha será mostrado os dados na tela
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		finally {
+			//Utilizamos os métodos da classe DB para fechar as instâncias feitas(Um método diferente para cada uma)
+			DB.closseResultSet(rs);
+			DB.closseStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,58 +4,86 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
-
 public class DB {
-	
-private static Connection conn = null;
-	
-    //Método para conectar ao banco de dados
+
+	private static Connection conn = null;
+
+	// Método para conectar ao banco de dados
 	public static Connection getConnection() {
-		//Se variável conn for igual a nulo
-		if(conn == null) {
+		// Se variável conn for igual a nulo
+		if (conn == null) {
 			try {
-				//Carregamos as conexões para variável props
+				// Carregamos as conexões para variável props
 				Properties props = loadProperties();
-				//A variável URL do tipo String recebe a propriedade dburl que está no arquivo db.properties
+				// A variável URL do tipo String recebe a propriedade dburl que está no arquivo
+				// db.properties
 				String url = props.getProperty("dburl");
-				//conn recebe um método que faz a leitura das propriedades URL e props
-				conn = DriverManager.getConnection(url,props);
-			}
-			catch(SQLException e) {
+				// conn recebe um método que faz a leitura das propriedades URL e props
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
 	}
-	
-	//Método para fechar conexão
+
+	// Método para fechar conexão
 	public static void closeConnection() {
-		//Se conn for diferente de null utilizamos o método close para fexhar a conexão com o banco de dados
-		if(conn != null) {
-			try{
+		// Se conn for diferente de null utilizamos o método close para fexhar a conexão
+		// com o banco de dados
+		if (conn != null) {
+			try {
 				conn.close();
-			}
-			catch(SQLException e){
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
-	//Método para guardar propriedades(Informações do banco de dados que estão na pasta db.properties)
+
+	// Método para guardar propriedades(Informações do banco de dados que estão na
+	// pasta db.properties)
 	private static Properties loadProperties() {
-		//Acessamos o arquivo db.properties
+		// Acessamos o arquivo db.properties
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			//Criamos(Instanciamos) uma variável do tipo Properties,chamada props.
+			// Criamos(Instanciamos) uma variável do tipo Properties,chamada props.
 			Properties props = new Properties();
-			//o método load armazena todas as propriedades dentro do arquivo na variável props
+			// o método load armazena todas as propriedades dentro do arquivo na variável
+			// props
 			props.load(fs);
 			return props;
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
-}
+	}
+
+	// Método para fechar instâncias Statement
+	public static void closseStatement(Statement st) {
+		// Se st for diferente de null o propraga tenta fechar a instância ou lança um
+		// erro
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// Método para fechar instâncias do tipo ResultSet
+	public static void closseResultSet(ResultSet rs) {
+		// Se st for diferente de null o propraga tenta fechar a instância ou lança um
+		// erro
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
