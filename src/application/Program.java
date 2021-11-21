5b19fae4ebecc4392b421ba799d953f4fa6afab5
Marcos_Alexandre <junior.marcos.mjr@hotmail.com>
2021-11-20 21:04:24 -0300

    Inserindo imagens

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,8 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 import db.dbIntegrityException;
@@ -13,26 +13,41 @@ public class Program {
 
 		// Representa uma conexão ao banco de dados
 		Connection conn = null;
-		// É usado para criar um objeto que representa a instrução SQL que será
-		// executada
-		PreparedStatement st = null;
+		// É usado para criar um objeto que representa a instrução SQL que será executada
+		Statement st = null;
 
 		try {
-			// Conexão com o banco de dados
 			conn = DB.getConnection();
-
-			st = conn.prepareStatement("DELETE FROM department " + "WHERE " + "Id = ?");
-
-			st.setInt(1, 5);
-
-			// Número de linhas que foram alteradas
-			int rowsAffected = st.executeUpdate();
-
-			System.out.println("Done! Rows affected: " + rowsAffected);
-
+			
+			//Através da função setAutoCommit não salvamos as operações automaticamente
+			conn.setAutoCommit(false);
+			
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+			
+			/*
+			int x =1;
+			if(x < 2) {
+				throw new SQLException("Fake ERROR!");
+			}*/
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			//A função .commit salva a operação realizada
+			conn.commit();
+			
+			System.out.println("rows1" + rows1);
+			System.out.println("rows2" + rows2);
+			
 		} catch (SQLException e) {
-			//Podemos capturar uma exceção personalizada
-			throw new dbIntegrityException(e.getMessage());
+			try {
+				//A função rollback() tenta retornar ao estado original do banco / passível a erro
+				conn.rollback();
+				throw new dbIntegrityException("Transaction rolled back! Caused by :" + e.getMessage());
+			} catch (SQLException e1) {
+				throw new dbIntegrityException("Error trying to rollback!  Caused by :" + e1.getMessage());
+			}
 
 		} finally {
 			DB.closseStatement(st);
